c695e4973fe76a948fd6f035e6808507568160dd
Atila Garcia Avila <atilagarcia12@gmail.com>
2025-11-08 00:33:00 -0300

    adiciona estrutura inicial do projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,10 @@ export default function Home() {
           priority
         />
         <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
+          <h1 className="h1 fw-bolder">
+            Icon: <i className="bi bi-fire"></i> To get started, edit the
+            page.tsx file.
+          </h1>
           <p>
             Looking for a starting point or more instructions? Head over to{" "}
             <a
